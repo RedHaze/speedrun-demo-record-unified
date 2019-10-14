6b2a1e1e7693feb834c53792c89729a79efeb686
Red Haze <redhaze3323@gmail.com>
2019-10-14 13:20:27 -0700

    Remove VPC files and crc checks, correct HL2 game dir variable

--- speedrun_demorecord_dev_env.bat
+++ speedrun_demorecord_dev_env.bat
@@ -9,7 +9,7 @@ set SDK_2013_DIR=E:\LLVM\source-sdk-2013\sp
 REM =-------------------------------=
 REM Games
 Rem =-------------------------------=
-set GAME_HL2_DIR=C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\hl2
+set GAME_HL2_DIR=C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2
 
 
 REM -- DO NOT EDIT BELOW THIS LINE --
